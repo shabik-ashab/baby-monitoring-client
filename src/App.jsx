b9f4ddf378e0cec0392cf4750ca6334219960f79
shabik-ashab <shabik.ashab@gmail.com>
2024-07-14 16:42:41 +0600

    intialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MonitoringBtn from "./components/MonitoringBtn"
+
+
+function App() {
+
+  return (
+    <>
+      <MonitoringBtn />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MonitoringBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MonitoringBtn = () => {
+  return (
+    <button className="btn btn-neutral">Neutral</button>
+  )
+}
+
+export default MonitoringBtn
